a1e2efcc690e1655e1a754ea3c08fc021fc9cc56
zoumson <stargue49@gmail.com>
2021-06-10 22:33:07 +0800

    add typedef

--- src/Demo.cpp
+++ src/Demo.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include "FctArgs.h"
 
-void function3Args(za::FctArgs<int, std::string,bool> arg)
+typedef za::FctArgs<int, std::string,bool> arg_3_isb;
+typedef za::FctArgs<int, std::string> arg_2_is;
+
+void function2Args(arg_2_is arg)
+{
+    std::cout << __PRETTY_FUNCTION__ << "\n";
+    std::cout <<"First arguemnt: "<< arg.arg1() <<"\n";
+    std::cout <<"Second arguemnt: "<< arg.arg2() <<"\n";        
+}
+void function3Args(arg_3_isb arg)
 {
     std::cout << __PRETTY_FUNCTION__ <<"\n";
     std::cout <<"First arguemnt: "<< arg.arg1() <<"\n";
@@ -9,17 +18,12 @@ void function3Args(za::FctArgs<int, std::string,bool> arg)
     std::cout <<"Third arguemnt: "<< arg.arg3() <<"\n";
 
 }
-void function2Args(za::FctArgs<int, std::string> arg)
-{
-    std::cout << __PRETTY_FUNCTION__ << "\n";
-    std::cout <<"First arguemnt: "<< arg.arg1() <<"\n";
-    std::cout <<"Second arguemnt: "<< arg.arg2() <<"\n";        
-}
+
 int main() {
     std::cout << "Demo FctArgs template class\n";
 
-    za::FctArgs<int, std::string> function2ArgsEx(100, "test 2 args");    
-    za::FctArgs<int, std::string,bool> function3ArgsEx(5, "test 3 args", true);
+    arg_2_is function2ArgsEx(100, "test 2 args");    
+    arg_3_isb function3ArgsEx(5, "test 3 args", true);
     function2Args(function2ArgsEx);
     function3Args(function3ArgsEx);
 
